0df81031a5232575e7bc880db8a59ebe32103132
Israel Barros <israel.silva.barros@hotmail.com>
2017-05-05 18:30:30 -0300

    Add files via upload

---
+++ 1078/Main.java
@@ -0,0 +1,19 @@
+package pkg1078;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        int i = 1;
+            n = sc.nextInt();
+
+            while (i <= 10) {
+                System.out.println(i + " x " + n + " = " + (n * i));
+                i++;
+            }
+
+
+    }
+    
+}
